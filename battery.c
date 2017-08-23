cd1fe1d0740f6b2577f94da20c93726bf7024445
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2017-08-23 19:29:29 +0300

    battery: read CHARGE_* values as well

--- battery.c
+++ battery.c
@@ -32,6 +32,10 @@ bool battery_get(int id, Battery *bt) {
   return r;
 }
 
+long mAh_to_mWh(long voltage, long val) {
+  return (voltage * val) / (1000 * 1000);
+}
+
 bool battery_get_from_file(char *file, Battery *bt) {
   FILE *fp = fopen(file, "r");
   if (!fp) return false;
@@ -41,7 +45,8 @@ bool battery_get_from_file(char *file, Battery *bt) {
 
   char *line = NULL;
   size_t line_size = 0;
-  int power = -1, energy = -1, energy_full = -1;
+  long voltage = -1, power = -1, energy = -1, energy_full = -1;
+  bool watt_as_unit = true;
   while (getline(&line, &line_size, fp) != -1) {
     char key[BUFSIZ], val[BUFSIZ];
     sscanf(line, "%[^=]=%s", key, val);
@@ -50,14 +55,35 @@ bool battery_get_from_file(char *file, Battery *bt) {
     if (strcmp("POWER_SUPPLY_STATUS", key) == 0
 	&& strcasecmp("charging", val) == 0) bt->is_charging = true;
     if (strcmp("POWER_SUPPLY_POWER_NOW", key) == 0) power = atoi(val);
-    if (strcmp("POWER_SUPPLY_ENERGY_NOW", key) == 0) energy = atoi(val);
-    if (strcmp("POWER_SUPPLY_ENERGY_FULL", key) == 0) energy_full = atoi(val);
     if (strcmp("POWER_SUPPLY_CAPACITY", key) == 0) bt->capacity = atoi(val);
+    if (strcmp("POWER_SUPPLY_ENERGY_NOW", key) == 0) {
+      watt_as_unit = true;
+      energy = atoi(val);
+    }
+    if (strcmp("POWER_SUPPLY_ENERGY_FULL", key) == 0) energy_full = atoi(val);
+
+    // mAh case
+    if (strcmp("POWER_SUPPLY_VOLTAGE_NOW", key) == 0) voltage = atoi(val);
+    if (strcmp("POWER_SUPPLY_CHARGE_NOW", key) == 0) {
+      watt_as_unit = false;
+      energy = atoi(val);
+    }
+    if (strcmp("POWER_SUPPLY_CHARGE_FULL", key) == 0) energy_full = atoi(val);
+    if (strcmp("POWER_SUPPLY_CURRENT_NOW", key) == 0) power = atoi(val);
 
     key[0] = '\0';
     val[0] = '\0';
   }
   free(line);
+
+  // ENERGY_* attrs represents capacity in mWh;
+  // CHARGE_* attrs represents capacity in mAh;
+  if (!watt_as_unit && voltage != -1) {
+    // actually this is not necessary
+    power = mAh_to_mWh(voltage, power);
+    energy = mAh_to_mWh(voltage, energy);
+    energy_full = mAh_to_mWh(voltage, energy_full);
+  }
 
   if (power > 0 && energy > 0 && energy_full > 0) {
     double hours;
@@ -69,6 +95,10 @@ bool battery_get_from_file(char *file, Battery *bt) {
     bt->seconds_remaining = (int)(hours * 60*60);
   } else
     bt->seconds_remaining = 0;
+
+  if (bt->capacity < 0 && energy > 0 && energy_full > 0) {
+    bt->capacity = ((double)energy/energy_full)*100;
+  }
 
   fclose(fp);
   return true;
